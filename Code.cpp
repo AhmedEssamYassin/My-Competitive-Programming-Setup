74cb9613316f77b6a87d8a0a8bdb2c293979ee88
AhmedEssamYassin <eng.ahmedessamyassin@gmail.com>
2026-01-07 23:49:26 +0200

    test: add a testing problem.

--- Code.cpp
+++ Code.cpp
@@ -1,6 +1,6 @@
 /*
 Author: Ahmed Yassin
-Created: 2025-11-26 11:21:54
+Created: 2026-01-07 23:36:09
 */
 #include <bits/stdc++.h>
 #ifndef ONLINE_JUDGE
@@ -25,20 +25,27 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
+	// freopen("input.txt", "r", stdin);
 	freopen("Output.txt", "w", stdout);
 #endif // ONLINE_JUDGE
 	int t = 1;
-	cin >> t;
+	// cin >> t;
 	while (t--)
 	{
-		int n;
-		cin >> n;
-		vector<ll> vc(n);
-		for (int i{}; i < n; i++)
-			cin >> vc[i];
-		debug(vc);
-		cout << (vc[0] < vc[n - 1] ? "YES\n" : "NO\n");
+		string str;
+		cin >> str;
+		ll cnt{}, cur{};
+		vector<int> seen(3, 0);
+		seen[0] = 1;
+		for (int i{}; i < str.length(); i++)
+		{
+			(cur += str[i] - '0') %= 3;
+			if (seen[cur])
+				cnt++, cur = 0, fill(seen.begin(), seen.end(), 0), seen[0] = 1;
+			else
+				seen[cur] = 1;
+		}
+		cout << cnt << endl;
 	}
 	return 0;
 }
